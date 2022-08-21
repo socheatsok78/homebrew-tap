c7170bb202b4bcff9f7c9d884d3ecb43053381e8
Socheat Sok <socheatsok78@gmail.com>
2022-08-21 09:45:39 +0700

    Create octoscreen.rb

---
+++ Casks/octoscreen.rb
@@ -0,0 +1,10 @@
+cask "octoscreen" do
+  version "1.0"
+  sha256 :no_check
+
+  url "https://github.com/socheatsok78-lab/octoscreen/releases/download/v1.0/Octoscreen.saver.zip"
+  name "octoscreen"
+  homepage "https://github.com/socheatsok78-lab/octoscreen/"
+
+  screen_saver "Octoscreen.saver"
+end
