b983ba71293210b2493c93b68aa9eda70b20dbc4
Socheat Sok <socheatsok78@gmail.com>
2025-09-02 15:36:04 +0700

    Update `sshpass` sha256
    Closes #15.
    
    Signed-off-by: Socheat Sok <socheatsok78@gmail.com>

--- Formula/sshpass.rb
+++ Formula/sshpass.rb
@@ -2,7 +2,7 @@ class Sshpass < Formula
   desc "Tool for non-interactivly performing password authentication with SSH"
   homepage "https://sourceforge.net/projects/sshpass"
   url "https://downloads.sourceforge.net/project/sshpass/sshpass/1.10/sshpass-1.10.tar.gz"
-  sha256 "71746e5e057ffe9b00b44ac40453bf47091930cba96bbea8dc48717dedc49fb7"
+  sha256 "ad1106c203cbb56185ca3bad8c6ccafca3b4064696194da879f81c8d7bdfeeda"
 
   def install
     system "./configure", "--disable-debug",
