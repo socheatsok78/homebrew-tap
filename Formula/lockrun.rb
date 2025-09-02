530291a8b0480bf545ceaadbffd0aab58ec8c58b
Socheat Sok <socheatsok78@gmail.com>
2025-09-02 08:55:50 +0000

    lockrun: add 1.1.3 bottle.

--- Formula/lockrun.rb
+++ Formula/lockrun.rb
@@ -13,6 +13,13 @@ class Lockrun < Formula
     regex(%r{v?(\d+(?:\.\d+)+)\s+\d{4}[/-]\d{2}[/-]\d{2}}i)
   end
 
+  bottle do
+    root_url "https://github.com/socheatsok78/homebrew-tap/releases/download/lockrun-1.1.3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "589305a6ffd21cfe832c5c092e15dcf0df9b3a6a6684d1182468c093a5d3054c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d7f491403cd267d0a1fb0f0acde1236cf50b265390d1117f3855d876eefb735c"
+    sha256 cellar: :any_skip_relocation, ventura:       "9d3172d2e1f1725b900e6b2c81545159aef2a4fd243867c0551b564f82b0fc1b"
+  end
+
   def install
     system ENV.cc, "lockrun.c", "-o", "lockrun"
     bin.install "lockrun"
