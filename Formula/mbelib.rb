a7c10ca1b21f4b838d6eec295050ae583fc10ee6
Socheat Sok <socheatsok78@gmail.com>
2025-09-02 07:42:07 +0000

    mbelib: update latest bottle.

--- Formula/mbelib.rb
+++ Formula/mbelib.rb
@@ -9,10 +9,9 @@ class Mbelib < Formula
 
   bottle do
     root_url "https://github.com/socheatsok78/homebrew-tap/releases/download/mbelib-latest"
-    rebuild 1
-    sha256 cellar: :any,                 arm64_sequoia: "29675ed6e7490223ae4b92782e18d233c4f71be0e9987717280b289fca36b2fb"
-    sha256 cellar: :any,                 ventura:       "6ea53528f0d0d2e9673dd9566539a650626316f68cffcf67a65a55d8b6aea074"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5d922cf5f44fb5ac1ac353d029f96f3ae595be3c5cea07204b4ff25a4c2de19e"
+    sha256 cellar: :any, arm64_sequoia: "f41dff62ae02596eb56125f7bd7780315602213c2a504159f9c0ddc8dec68352"
+    sha256 cellar: :any, arm64_sonoma:  "68ea3feb32f071d3950f4d31f78337248aa99739dff6fbf8bdcf5cb671dda0a6"
+    sha256 cellar: :any, ventura:       "44ccd296c5d59f6f7f7e185dccd6bea39583ff68c0231c9b87e357c41d921064"
   end
 
   depends_on "cmake" => :build
