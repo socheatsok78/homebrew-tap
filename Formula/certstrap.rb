fe97250963dd37e40be4c9c3cc4904265f3e1999
Socheat Sok <socheatsok78@gmail.com>
2025-09-02 15:08:56 +0700

    Add `certstrap` formula
    Closes #12.
    
    Signed-off-by: Socheat Sok <socheatsok78@gmail.com>

---
+++ Formula/certstrap.rb
@@ -0,0 +1,19 @@
+class Certstrap < Formula
+  desc "Tools to bootstrap CAs, certificate requests, and signed certificates"
+  homepage "https://github.com/socheatsok78/certstrap"
+  url "https://github.com/socheatsok78/certstrap/archive/refs/heads/master.tar.gz"
+  version "latest"
+  sha256 "f3ca1d642e0e59ceea5f5cd44aa049fbeed67ebde1051f0dd35cd0f3573a6884"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-ldflags", "-s -w -X main.release=#{version}", "-trimpath", "-o", bin/"certstrap"
+    prefix.install_metafiles
+  end
+
+  test do
+    system "#{bin}/certstrap", "init", "--common-name", "Homebrew Test CA", "--passphrase", "beerformyhorses"
+  end
+end
