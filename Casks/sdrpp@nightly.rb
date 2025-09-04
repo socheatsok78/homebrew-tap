c9d4264345f4e02ce450e268d5ac0d6df0d8b0f9
Socheat Sok <socheatsok78@gmail.com>
2025-09-04 17:55:36 +0700

    renamed:    Casks/sdrpp.rb -> Casks/sdrpp@nightly.rb

--- Casks/sdrpp@nightly.rb
+++ Casks/sdrpp@nightly.rb
@@ -1,9 +1,8 @@
 cask "sdrpp" do
   arch arm: "arm", intel: "intel"
 
-  version "1.2.1"
-  sha256 arm:   "da9db2f998421b96876e4f8cced310050e1aabf4ac295eed8dfbd17275348744",
-         intel: "0efed2a5bdf7b0ac9ec99f1981b4bc3437cc6185676f080b39ef758c69d6ad11"
+  version "nightly"
+  sha256 :no_check
 
   url "https://github.com/AlexandreRouma/SDRPlusPlus/releases/download/nightly/sdrpp_macos_#{arch}.zip"
   name "sdrpp"
