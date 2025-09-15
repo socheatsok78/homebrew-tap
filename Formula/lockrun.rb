ba640eacfece0c540245b38f6e18de4fc8753f19
Socheat Sok <socheatsok78@gmail.com>
2025-09-15 09:08:46 +0000

    lockrun: update 1.1.3 bottle.

--- Formula/lockrun.rb
+++ Formula/lockrun.rb
@@ -15,10 +15,10 @@ class Lockrun < Formula
 
   bottle do
     root_url "https://github.com/socheatsok78/homebrew-tap/releases/download/lockrun-1.1.3"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "589305a6ffd21cfe832c5c092e15dcf0df9b3a6a6684d1182468c093a5d3054c"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d7f491403cd267d0a1fb0f0acde1236cf50b265390d1117f3855d876eefb735c"
-    sha256 cellar: :any_skip_relocation, ventura:       "9d3172d2e1f1725b900e6b2c81545159aef2a4fd243867c0551b564f82b0fc1b"
-    # tahoe:
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b1b29adc41b4669f7c5198aa6f6b152770b1d9b0898382dca775637cdb0fb4b2"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "718f1132444985b448f747e6088efb8ca0df5d4531824ad6bcc7417711c8ff99"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "de82646903ffa48280f1d737e73dacd1c3f359b45e4eb6ff8cf15b58ea7593f5"
   end
 
   def install
