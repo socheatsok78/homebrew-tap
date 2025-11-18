f777a3f18809e31da2dbf95a966bd9d6be84d8e9
Socheat Sok m <github@socheat.dev>
2025-11-18 13:34:32 +0000

    gon: add 0.2.5 bottle.

--- Formula/gon.rb
+++ Formula/gon.rb
@@ -6,6 +6,13 @@ class Gon < Formula
   sha256 "b2a2f744bb72aaf396399c75b0215078b6ce9d5519644bfce3a2a05f2f02923a"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/socheatsok78/homebrew-tap/releases/download/gon-0.2.5"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2f483c95c82d0f39198cb202d9a1b98fbf2074a3abff2706c33abe64cd321cf3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "92fb86533b04e894f216162e258fde18a52627ec5f82cd56cbff95341389c301"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "486dbf51fd2c5294c83db5ab9ff3f2338cbd22df11c1a471b81d5612e0b9fe76"
+  end
+
   depends_on "go" => :build
   depends_on :macos
 
