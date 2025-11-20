2121528752fd2ab9e431c777ce0443c7fe0df265
Socheat Sok <github@socheat.dev>
2025-11-20 17:13:16 +0700

    Migrate `gon` patches to use `socheatsok78-lab:go-notarize`
    Closes #49.
    
    Signed-off-by: Socheat Sok m <github@socheat.dev>

--- Formula/gon.rb
+++ Formula/gon.rb
@@ -18,12 +18,8 @@ class Gon < Formula
   depends_on :macos
 
   patch do
-    url "https://github.com/mitchellh/gon/compare/master...winash:go-notarize:master.patch"
-    sha256 "b6d8a86aa069c119f960a37cec61428fa5b90ec3ddb5c6b679a6a6775ea86e00"
-  end
-  patch do
-    url "https://github.com/winash/go-notarize/compare/master...socheatsok78-lab:go-notarize:master.patch"
-    sha256 "84473fa5848e30e1709d8ae4160bdac45f386ae41981997684c4c4f9ab6e5f72"
+    url "https://github.com/mitchellh/gon/compare/master...socheatsok78-lab:go-notarize:master.patch"
+    sha256 "3b31f090b1902888b76b68d476c585d7678a3fd7a56de2c6bcee32a26bfe21bf"
   end
 
   def install
