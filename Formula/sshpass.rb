00af0168deb22c99431a552fccb6250dc568fb8c
Socheat <socheatsok78@gmail.com>
2025-12-02 10:10:37 +0700

    Enhance sshpass.rb with license and usage caveats
    Added license information and caveats for usage. Updated install method to use standard configure arguments.
    
    Closes #50.
    
    Signed-off-by: Socheat <github@socheat.dev>

--- Formula/sshpass.rb
+++ Formula/sshpass.rb
@@ -3,6 +3,7 @@ class Sshpass < Formula
   homepage "https://sourceforge.net/projects/sshpass"
   url "https://downloads.sourceforge.net/project/sshpass/sshpass/1.10/sshpass-1.10.tar.gz"
   sha256 "ad1106c203cbb56185ca3bad8c6ccafca3b4064696194da879f81c8d7bdfeeda"
+  license "GPL-2.0-only"
 
   bottle do
     root_url "https://github.com/socheatsok78/homebrew-tap/releases/download/sshpass-1.10"
@@ -13,13 +14,22 @@ class Sshpass < Formula
   end
 
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    system "./configure", *std_configure_args
     system "make", "install"
   end
 
+  def caveats
+    <<~EOS
+      Sshpass is a tool for non-interactively performing password authentication
+      with SSH's so called "interactive keyboard password authentication".
+      Most users should use SSH's more secure public key authentication instead.
+
+      See `man sshpass` for more information.
+    EOS
+  end
+
   test do
-    system "true"
+    output = shell_output("#{bin}/sshpass -P password ssh foo@bar ls 2>&1", 255)
+    assert_match(/ssh: Could not resolve hostname bar/, output)
   end
 end
