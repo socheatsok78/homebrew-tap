ed6e0bdab4bf166762f245d35abf5cdacacd6d4a
Socheat <github@socheat.dev>
2025-12-16 10:46:04 +0000

    m17-fme: update 2025-11-28 bottle.

--- Formula/m17-fme.rb
+++ Formula/m17-fme.rb
@@ -8,10 +8,10 @@ class M17Fme < Formula
   head "https://github.com/lwvmobile/m17-fme.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/socheatsok78/homebrew-tap/releases/download/m17-fme-2025-11-11"
-    sha256 cellar: :any, arm64_tahoe:   "53b044f5e9a6518c7de958ec34b5e6ae59746e2ab3980e3f542e7eee17cbbc3c"
-    sha256 cellar: :any, arm64_sequoia: "8a49b4eba40c5bde274d1e29520138b4742d8f1b43f146ee712e1ddbd5787ce1"
-    sha256 cellar: :any, arm64_sonoma:  "9806c624784a274e43c36b7a2a40833e6c117c836c719e6b636c1be7a0054c14"
+    root_url "https://github.com/socheatsok78/homebrew-tap/releases/download/m17-fme-2025-11-28"
+    sha256 cellar: :any, arm64_tahoe:   "f85c994880e8d443ebf5f1bfc0d0446ae58c6e52a2146d278be189e1d105c4b1"
+    sha256 cellar: :any, arm64_sequoia: "c7bbb833615104341c319999c72b8ce58f326807c4f6e416819c4dc1c7d6e116"
+    sha256 cellar: :any, arm64_sonoma:  "0f1b9a6bf7a585772e11fa0ac710cf0bc2593f942a7d7ff08d8647ac62169ac5"
   end
 
   depends_on "cmake" => :build
