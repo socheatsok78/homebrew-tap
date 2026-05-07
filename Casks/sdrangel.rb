665ea1e704f291b980979f720b4f188c4144ca7d
Socheat <github@socheat.dev>
2026-05-07 16:45:52 +0700

    Bump `sdrangel` to v7.25.1

--- Casks/sdrangel.rb
+++ Casks/sdrangel.rb
@@ -1,9 +1,9 @@
 cask "sdrangel" do
-  arch arm: "-480-g11074ac87_mac-14.7.6_arm64", intel: "_mac-13.7.6_x86_64"
+  arch arm: "_mac-14.8.5_arm64", intel: "_mac-15.7.5_x86_64"
 
-  version "7.22.9"
-  sha256 arm:   "082bb21cfa74ed56a82c928c82f8a418ef9852bb5d542797e4d0ef49e20aa2c8",
-         intel: "af12179ca22ed510bbef9f35e690f599b5333c7dda469c9cc077bcbf94bc3e39"
+  version "7.25.1"
+  sha256 arm:   "08faf0ae91d3343dc8809366e464a84f0af6bf7152beba0124e654d4a65d2272",
+         intel: "b2f64072c48903e03b58b1c78ca9ee47d2a84884fd7640bfc01ea3a9eb006e4a"
 
   url "https://github.com/f4exb/sdrangel/releases/download/v#{version}/sdrangel-#{version}#{arch}.dmg",
       verified: "github.com/f4exb/sdrangel/"
