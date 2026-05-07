2a3ef320e2d87f2f75dc9fb48e8e86f8c4e5d829
Socheat <github@socheat.dev>
2026-05-07 16:58:56 +0700

    Update `chirp@next` release url again

--- Casks/chirp@next.rb
+++ Casks/chirp@next.rb
@@ -2,14 +2,14 @@ cask "chirp@next" do
   version "20260501"
   sha256 "c3a08ff26d2e251e751b0c2880639e0bdfc70ef4672a09e1e4a7ca8aff7d71e2"
 
-  url "https://github.com/socheatsok78/chirpmyradio-macos-archive/releases/download/next-#{version}/chirp-next-#{version}.app.zip",
-      verified: "github.com/socheatsok78/chirpmyradio-macos-archive/"
+  url "https://github.com/homebrew-sdr/chirpmyradio-archive/releases/download/next-#{version}/chirp-next-#{version}.app.zip",
+      verified: "github.com/homebrew-sdr/chirpmyradio-archive/"
   name "CHIRP"
   desc "Tool for programming amateur radio"
   homepage "https://chirp.danplanet.com/projects/chirp/wiki/Home"
 
   livecheck do
-    url "https://github.com/socheatsok78/chirpmyradio-macos-archive"
+    url "https://github.com/homebrew-sdr/chirpmyradio-archive"
     regex(/v?(\d+(?:\.\d+)*)/i)
   end
 
