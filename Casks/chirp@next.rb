ec98c77ea8e88c67c9d6a0b9ead2da6b25fc8c95
Socheat <github@socheat.dev>
2026-05-07 16:56:47 +0700

    fix `Casks/chirp@next.rb` syntax

--- Casks/chirp@next.rb
+++ Casks/chirp@next.rb
@@ -3,7 +3,7 @@ cask "chirp@next" do
   sha256 "c3a08ff26d2e251e751b0c2880639e0bdfc70ef4672a09e1e4a7ca8aff7d71e2"
 
   url "https://github.com/socheatsok78/chirpmyradio-macos-archive/releases/download/next-#{version}/chirp-next-#{version}.app.zip",
-      verified:   "github.com/socheatsok78/chirpmyradio-macos-archive/"
+      verified: "github.com/socheatsok78/chirpmyradio-macos-archive/"
   name "CHIRP"
   desc "Tool for programming amateur radio"
   homepage "https://chirp.danplanet.com/projects/chirp/wiki/Home"
