b0eab70a33e3f16c6296494e421d1c7a44c817a9
Socheat <github@socheat.dev>
2026-05-07 16:40:59 +0700

    Change `chirp@next` release to custom repo

--- Casks/chirp@next.rb
+++ Casks/chirp@next.rb
@@ -2,17 +2,15 @@ cask "chirp@next" do
   version "20260501"
   sha256 "c3a08ff26d2e251e751b0c2880639e0bdfc70ef4672a09e1e4a7ca8aff7d71e2"
 
-  url "https://archive.chirpmyradio.com/chirp_next/next-#{version}/chirp-next-#{version}.app.zip",
-      verified:   "archive.chirpmyradio.com/",
-      referer:    "https://archive.chirpmyradio.com/chirp_next/next-#{version}/",
-      user_agent: :fake
+  url "https://github.com/socheatsok78/chirpmyradio-macos-archive/releases/download/#{version}/chirp-next-#{version}.app.zip",
+      verified:   "github.com/socheatsok78/chirpmyradio-macos-archive/"
   name "CHIRP"
   desc "Tool for programming amateur radio"
   homepage "https://chirp.danplanet.com/projects/chirp/wiki/Home"
 
   livecheck do
-    url "https://archive.chirpmyradio.com/chirp_next/"
-    regex(/next[._-]v?(\d+(?:\.\d+)*)/i)
+    url "https://github.com/socheatsok78/chirpmyradio-macos-archive"
+    regex(/v?(\d+(?:\.\d+)*)/i)
   end
 
   conflicts_with cask: "chirp"
