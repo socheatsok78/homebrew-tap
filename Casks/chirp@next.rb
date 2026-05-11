16e669c8b43b92a9e8ca9390cfde3cf28160128d
Socheat <github@socheat.dev>
2026-05-11 15:49:32 +0700

    Update chirp@next to v20260508

--- Casks/chirp@next.rb
+++ Casks/chirp@next.rb
@@ -1,6 +1,6 @@
 cask "chirp@next" do
-  version "20260501"
-  sha256 "c3a08ff26d2e251e751b0c2880639e0bdfc70ef4672a09e1e4a7ca8aff7d71e2"
+  version "20260508"
+  sha256 "6d0955edd607f112dee08e77e14d04970bcdbf2403dc7944a7d935a6271513cb"
 
   url "https://github.com/homebrew-sdr/chirpmyradio-archive/releases/download/next-#{version}/chirp-next-#{version}.app.zip",
       verified: "github.com/homebrew-sdr/chirpmyradio-archive/"
