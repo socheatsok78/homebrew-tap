197e119b6acd2326b32ea37ae66fb60d73639724
Socheat <github@socheat.dev>
2026-06-27 11:51:06 +0700

    Add sdrpp-brown cask

---
+++ Casks/sdrpp-brown.rb
@@ -0,0 +1,16 @@
+cask "sdrpp-brown" do
+  arch arm: "arm", intel: "intel"
+
+  version "latest"
+  sha256 :no_check
+
+  url "https://github.com/sannysanoff/SDRPlusPlusBrown/releases/download/stable/sdrpp_macos_#{arch}.zip"
+  name "SDR++Brown"
+  desc "Cross-Platform SDR Software (Brown Edition)"
+  homepage "https://github.com/sannysanoff/SDRPlusPlusBrown"
+
+  conflicts_with cask: "sdrpp-brown@nightly"
+  suite "SDR++Brown.app"
+
+  zap trash: "~/Library/Application Support/sdrpp-brown"
+end
