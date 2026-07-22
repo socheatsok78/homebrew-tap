8506033de733e8a95a0c0c58875e23a9affd1456
Socheat <github@socheat.dev>
2026-07-22 16:51:22 +0700

    Add font-vegur cask

---
+++ Casks/font-vegur.rb
@@ -0,0 +1,20 @@
+cask "font-vegur" do
+  version "0701"
+  sha256 "cfb06f0860aae4a6ed0ffaba4ab749a7c09c7f482e68a284a6f832c8382669ed"
+
+  url "https://dotcolon.net/files/fonts/vegur_#{version}.zip"
+  name "font-vegur"
+  desc "Vegur is a humanist sans-serif font developed for the dot colon logotype"
+  homepage "https://dotcolon.net/fonts/vegur"
+
+  livecheck do
+    skip "No version information available"
+  end
+
+
+  font "Vegur-Bold.otf"
+  font "Vegur-Light.otf"
+  font "Vegur-Regular.otf"
+
+  # No zap stanza required
+end
